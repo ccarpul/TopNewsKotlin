24fffcbc4f8af0261acccfda915a9aaa1737b476
Carlos Pulido <carlosepulidor@gmail.com>
2020-06-29 17:16:54 -0300

    Reorganizando archivo de Extensiones

--- app/src/main/java/com/example/topnewsmvvmkotlin/util/ViewExtensions.kt
+++ app/src/main/java/com/example/topnewsmvvmkotlin/util/ViewExtensions.kt
@@ -1,7 +1,15 @@
 package com.example.topnewsmvvmkotlin.util
 
+import android.content.Context
+import android.graphics.Color
+import android.view.Gravity
 import android.view.View
+import android.widget.LinearLayout
 import android.widget.Spinner
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.topnewsmvvmkotlin.R
 import com.example.topnewsmvvmkotlin.ui.adapter.FilterAdapterSpinner
 import com.example.topnewsmvvmkotlin.ui.filters.ModelSpinner
@@ -19,6 +27,14 @@ val flagCountries = intArrayOf(
     R.drawable.ic_de, R.drawable.ic_fr, R.drawable.ic_gb, R.drawable.ic_it, R.drawable.ic_mx,
     R.drawable.ic_pt, R.drawable.ic_us, R.drawable.ic_ve)
 
+
+fun View.builtSpinners(){
+    spinnerFilterCountry .built(R.array.countries , flagCountries)
+    spinnerFilterCategory.built(R.array.categories, flagCountries)
+    spinnerFilterSource  .built(R.array.sources   , imageSources)
+    spinnerFilterLanguage.built(R.array.languages , flagLanguages)
+}
+
 fun Spinner.built(arraySpinnerNames: Int, listImages: IntArray){
 
     val listFilterSpinner: ArrayList<ModelSpinner> = arrayListOf()
@@ -27,13 +43,6 @@ fun Spinner.built(arraySpinnerNames: Int, listImages: IntArray){
         listFilterSpinner.add( ModelSpinner(itemsSpinnerName[item], listImages[item]) )
     }
     this.adapter = FilterAdapterSpinner(context, listFilterSpinner)
-}
-
-fun View.builtSpinners(){
-        spinnerFilterCountry .built(R.array.countries , flagCountries)
-        spinnerFilterCategory.built(R.array.categories, flagCountries)
-        spinnerFilterSource  .built(R.array.sources   , imageSources)
-        spinnerFilterLanguage.built(R.array.languages , flagLanguages)
 }
 
 fun Spinner.getValue(spinnerValues: Int):String{
@@ -45,9 +54,33 @@ fun Spinner.getValue(spinnerValues: Int):String{
 
 fun Spinner.hide(){
     this.apply{ visibility = View.INVISIBLE
-                setSelection(0)
-    }
+                setSelection(0) }
 }
 
 fun Spinner.show(){ this.visibility = View.VISIBLE }
 
+fun RecyclerView.isLastArticleDisplayed(linearLayoutManager: LinearLayoutManager): Boolean {
+
+    val totalItems = this.adapter?.itemCount
+    if (totalItems != 0) {
+        if (RecyclerView.NO_POSITION != linearLayoutManager.findLastCompletelyVisibleItemPosition() &&
+            linearLayoutManager.findLastCompletelyVisibleItemPosition() == totalItems?.minus(1)
+        )
+            return true
+    }
+    return false
+}
+
+fun makeToast(context: Context?, message: String) {
+
+    val toast = Toast.makeText(context, message, Toast.LENGTH_LONG)
+    val layout = toast.view as LinearLayout
+    layout.setBackgroundResource(R.drawable.shape_toast)
+    val viewMessage = layout.getChildAt(0) as TextView
+    viewMessage.setTextColor(Color.WHITE)
+    viewMessage.textSize = 16f
+    toast.apply {
+        setGravity(Gravity.BOTTOM, 0, 200)
+        show()
+    }
+}
